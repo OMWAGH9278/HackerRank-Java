4292b6ab080c6102009999cef97394e05e9dcbd7
Om Wagh <omwaghwaghpatta@gmail.com>
2022-12-06 13:02:51 +0530

    06/12/2022

---
+++ Solution13.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.text.*;
+
+public class Solution 
+{
+    public static void main(String arg[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        double payment = sobj.nextDouble();
+        sobj.close();
+        
+        // Write your code here.
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
